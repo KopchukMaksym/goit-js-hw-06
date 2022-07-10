06cb48e625c354926207e14d589d208ce6a21fcd
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-07-10 18:29:50 +0300

    10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const inputEl = document.querySelector("input");
+const createBtnEl = document.querySelector("[data-create]");
+const destroyBtnEl = document.querySelector("[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+const onCreateDivs = () => {
+    const divsEl = [];
+    let size = 30;
+
+    for (let i = 1; i <= Number(inputEl.value); i += 1) {
+        const randomColor = getRandomHexColor();
+        const div = `<div style="background-color:${randomColor};width:${size}px;height:${size}px">1</div>`;
+        size += 10;
+        divsEl.push(div);
+    }
+    boxesEl.innerHTML = divsEl.join("");
+};
+
+const onDestroyDivs = () => {
+    boxesEl.innerHTML = "";
+    inputEl.value = 1;
+};
+
+createBtnEl.addEventListener("click", onCreateDivs);
+destroyBtnEl.addEventListener("click", onDestroyDivs);
